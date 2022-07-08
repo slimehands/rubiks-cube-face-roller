1e3941a9917b90ba2e8a7a3c3b28aa6e4f2e5d2c
blastoff321 <rodriguesorlen@gmail.com>
2022-07-08 17:53:29 -0500

    Initial commit

---
+++ src/lib/RubiksFace.svelte
@@ -0,0 +1,10 @@
+<script>
+export let color
+</script>
+{#if color == "w"}
+{:else if color == "y"}
+{:else if color == "g"}
+{:else if color == "o"}
+{:else if color == "b"}
+{:else if color == "r"}
+{/if}
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+import {onMount} from 'svelte'
+import RubiksFace from '$lib/RubiksFace.svelte'
+const cubes = [["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"],
+["w","y","g","o","b","r"]]
+
+var str = ''
+
+function findCube(cube){
+	let rjekfo = cube[(Math.floor(Math.random()*cube.length))]
+	str = str + rjekfo
+	return rjekfo
+}
+</script>
+
+{#each cubes as cube}
+		<RubiksCube color={findCube(cube)}>
+{/each}
+
+{str}
